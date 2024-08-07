337522f9d5e929e22790b87c4d620fca3e126234
Nyco-T <tauschenico@gmail.com>
2024-08-07 13:19:06 +0200

    first

--- src/main/java/mercatis/com/Geschaeftswagen.java
+++ src/main/java/mercatis/com/Geschaeftswagen.java
@@ -1,7 +1,5 @@
 package mercatis.com;
 
-
-
 public class Geschaeftswagen {
     private int fortlaufendenummer;
     private String titel;
@@ -18,42 +16,5 @@ public class Geschaeftswagen {
         this.parkplatz = parkplatz;
 
     }
-    public int getFortlaufendenummer() {return fortlaufendenummer;}
-    public void setFortlaufendenummer(int fortlaufendenummer) {
-        this.fortlaufendenummer = fortlaufendenummer;
-    }
-    public String getTitel() {
-        return titel;
-    }
-    public void setTitel(String titel) {
-        this.titel = titel;
-    }
-    public String getBeschreibung() {
-        return beschreibung;
-    }
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-    }
-    public int getKmStand() {return kmStand;}
-    public void setKmStand(int kmStand) {
-        this.kmStand = kmStand;
-    }
-    public String getParkplatz() {
-        return parkplatz;
-    }
-    public void setParkplatz(String parkplatz) {
-        this.parkplatz = parkplatz;
-    }
-
-
-    public void anzeigen() {
-        System.out.println("Nummer: " + fortlaufendenummer);
-        System.out.println("Name des Autos: " + titel);
-        System.out.println("Beschreibung: " + beschreibung);
-        System.out.println("Kilometerstand: " + kmStand);
-        System.out.println("Standort des Autos: " + parkplatz);
-    }
-
-
 
 }
--- src/main/java/mercatis/com/Notebook.java
+++ src/main/java/mercatis/com/Notebook.java
@@ -1,7 +1,5 @@
 package mercatis.com;
 
-import java.util.Date;
-
 public class Notebook {
     private int fortlaufendenummer;
     private String titel;
@@ -9,8 +7,7 @@ public class Notebook {
     private int speicherplatz;
     private String prozessor;
 
-    public Notebook(int fortlaufendenummer, String titel, String beschreibung, int speicherplatz, String prozessor)
-    {
+    public Notebook(int fortlaufendenummer, String titel, String beschreibung, int speicherplatz, String prozessor) {
         this.fortlaufendenummer = fortlaufendenummer;
         this.titel = titel;
         this.beschreibung = beschreibung;
@@ -18,42 +15,5 @@ public class Notebook {
         this.prozessor = prozessor;
 
     }
-    public int getFortlaufendenummer() {return fortlaufendenummer;}
-
-    public void setFortlaufendenummer(int fortlaufendenummer) {
-        this.fortlaufendenummer = fortlaufendenummer;
-    }
-    public String getTitel() {
-        return titel;
-    }
-    public void setTitel(String titel) {
-        this.titel = titel;
-    }
-    public String getBeschreibung() {
-        return beschreibung;
-    }
-    public void setBeschreibung(String beschreibung) {
-        this.beschreibung = beschreibung;
-    }
-    public int getSpeicherplatz() {return speicherplatz;}
-    public void setSpeicherplatz(int speicherplatz) {
-        this.speicherplatz = speicherplatz;
-    }
-    public String getProzessor() {
-        return prozessor;
-    }
-    public void setProzessor(String prozessor) {
-        this.prozessor = prozessor;
-    }
-    public void darstellen() {
-        System.out.println("Nummer: " + fortlaufendenummer);
-        System.out.println("Titel: " + titel);
-        System.out.println("Beschreibung: " + beschreibung);
-        System.out.println("Größe des Speichers: " + speicherplatz);
-        System.out.println("Sprache: " + prozessor);
-
-    }
-
-
 
 }
